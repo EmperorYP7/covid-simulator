9e663810e5aeeb8d36ce081f43e4bd6f465fa39a
Yash Pandey (YP) <yash.btech.cs19@iiitranchi.ac.in>
2021-04-18 19:38:03 +0530

    fixed population param

--- virus_test.py
+++ virus_test.py
@@ -13,7 +13,8 @@ class TestVirus(unittest.TestCase):
             "severe_recovery": (21, 42),
             "severe_death": (14, 56),
             "fatality_rate": 0.034,
-            "serial_interval": 7
+            "serial_interval": 7,
+            "population": 10
         }
         self.assertRaises(ValueError, virus.Virus, CUSTOM_PARAMS)
 
